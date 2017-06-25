57790680c5ac460de091f0472ea14259b2679458
yddgit <642444906@qq.com>
2017-06-25 23:17:56 +0800

    Commit ReentrantLock sample

---
+++ src/main/java/com/my/project/T20ReentrantLock1.java
@@ -0,0 +1,39 @@
+package com.my.project;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * ReentrantLock用于替代synchronized
+ * 本例中由于m1锁定this，只有m1执行完毕的时候，m2才能执行
+ * 这里是复习synchronized最原始的语义
+ * @author yang
+ *
+ */
+public class T20ReentrantLock1 {
+
+	synchronized void m1() {
+		for(int i=0; i<10; i++) {
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(i);
+		}
+	}
+
+	synchronized void m2() {
+		System.out.println("m2 ...");
+	}
+
+	public static void main(String[] args) {
+		T20ReentrantLock1 t = new T20ReentrantLock1();
+		new Thread(t::m1).start();
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		new Thread(t::m2).start();
+	}
+}
